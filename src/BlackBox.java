91b255a7431ae853822421ad3007dd197da9718b
heroadn <benbenjamin554@gmail.com>
2023-09-23 19:17:50 -0300

    simplificado classe de testes

--- src/BlackBox.java
+++ src/BlackBox.java
@@ -3,6 +3,7 @@ public final class BlackBox {
 
     public static void main(String[] args)
     {
+        //automato nao deterministico
         {
             Automata nfa = new Automata();
             String line = "ab";
@@ -13,21 +14,46 @@ public final class BlackBox {
             nfa.addState(2);
 
             //Transições
-            nfa.addTransition(0, 1, "a");
-            nfa.addTransition(0, 2, "a");
-            nfa.addTransition(1, 2, "b");
+            nfa.addTransition(0, 1, "a"); //0 --- a ---> 1
+            nfa.addTransition(0, 2, "a"); //0 --- a ---> 2
+            nfa.addTransition(1, 2, "b"); //1 --- b ---> 2
 
+            //debug
+            System.out.println("NIER TRANSITIONS");
             nfa.getTransitions().forEach(System.out::println);
 
             //Iniciais e Finais
             nfa.setStartState(0);
             nfa.setFinalState(2);
+            System.out.println("IS_NFA::NFA = " + nfa.isNFA());
+        }
 
-            System.out.println("IS_NFA::NFA: " + nfa.isNFA());
-            if(nfa.recognize(line))
-                System.out.println("DEBUG::NFA::SUCCESS");
+        {
+            Automata nier = new Automata();
+            String line = "abcd";
+
+            //Estados
+            nier.addState(0);
+            nier.addState(1);
+            nier.addState(2);
+            nier.addState(3);
+            nier.addState(4);
+
+            //Transições
+            nier.addTransition(0, 1, "a");
+            nier.addTransition(1, 2, "b");
+            nier.addTransition(2, 3, "c");
+            nier.addTransition(3, 4, "d");
+
+            //Iniciais e Finais
+            nier.setStartState(0);
+            nier.setFinalState(4);
+
+            System.out.print("RECOGNIZES::NIER = ");
+            if(nier.recognize(line))
+                System.out.println("SUCCESS");
             else
-                System.out.println("DEBUG::NFA::FAILED");
+                System.out.println("FAILED");
         }
 
         //com loop no estado 1
@@ -49,11 +75,11 @@ public final class BlackBox {
             a2.setStartState(0);
             a2.setFinalState(2);
 
-            System.out.println("IS_NFA::A2: " + a2.isNFA());
+            System.out.print("RECOGNIZES::A2 = ");
             if(a2.recognize(line))
-                System.out.println("DEBUG::A2::SUCCESS");
+                System.out.println("SUCCESS");
             else
-                System.out.println("DEBUG::A2::FAILED");
+                System.out.println("FAILED");
         }
     }
 }
